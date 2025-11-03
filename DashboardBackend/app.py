60c32690a43978f34c084e8c013b13852a20dad4
Adogamm <1718110396@utectulancingo.edu.mx>
2025-11-03 01:04:09 -0600

    Token validation on back

--- DashboardBackend/app.py
+++ DashboardBackend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 CORS(app)
@@ -23,6 +23,12 @@ def login():
         print(f"Usuario {username} ha iniciado sesión.")
         return jsonify({"message": "Login exitoso", "token": token})
     return jsonify({"message": "Credenciales inválidas"}), 401
+
+@app.route('/dashboard', methods=['GET'])
+@jwt_required()
+def dashboard():
+    user = get_jwt_identity()
+    return jsonify({"message": f"Bienvenido al dashboard, {user}!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
